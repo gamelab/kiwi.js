aa7091d747b58e59e089589c56a9a718312aa315
Zachary Freiberg <zachary.freiberg@gmail.com>
2013-07-24 10:13:06 +1200

    Switching and creating animations created

---
+++ examples/animations/CreatingAndSwitchingAnimations.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../src/Kiwi.ts" />
+
+class CreatingAndSwitchingAnimations extends Kiwi.State {
+
+    constructor() {
+        super("CreatingAndSwitchingAnimations");
+    }
+
+    public sprite: Kiwi.GameObjects.Sprite;
+
+    preload() {
+
+        this.addSpriteSheet('coin', 'assets/coin.png', 32, 32);
+    }
+
+    create() {
+
+        this.sprite = new Kiwi.GameObjects.Sprite('coin', this.game.cache, 100, 100);
+        this.sprite.animation.add('rotateRight', 0.1, [0, 1, 2, 3, 4, 5], Kiwi.Anims.PLAY_LOOP);
+        this.sprite.animation.add('rotateLeft', 0.1, [5, 4, 3, 2, 1, 0], Kiwi.Anims.PLAY_LOOP);
+        this.sprite.animation.play('rotateRight');
+        this.addChild(this.sprite);
+    }
+
+    update() {
+
+
+    }
+
+}
+
