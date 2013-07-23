b22b97c173904b858adcb54eb044b72798210cc2
Lachlan Reid <locky_reid906hotmail.com>
2013-07-24 11:14:47 +1200

    Changes made to the component examples. Removed the uneccessary example

--- examples/components/AlphaComponent.ts
+++ examples/components/AlphaComponent.ts
@@ -10,6 +10,7 @@
 
         zombie: Kiwi.GameObjects.Sprite;
         zombie2: Kiwi.GameObjects.Sprite;
+        zombie3: Kiwi.GameObjects.Sprite;
 
         preload() {
 
@@ -18,20 +19,26 @@
 
         create() {
 
-            this.zombie = new Kiwi.GameObjects.Sprite('zombie', this.game.cache, 33, 40);
-            this.zombie2 = new Kiwi.GameObjects.Sprite('zombie', this.game.cache, 216, 40);
+            this.zombie = new Kiwi.GameObjects.Sprite('zombie', this.game.cache, 7, 40);
+            this.zombie2 = new Kiwi.GameObjects.Sprite('zombie', this.game.cache, 138, 40);
+            this.zombie3 = new Kiwi.GameObjects.Sprite('zombie', this.game.cache, 269, 40);
 
             this.zombie.alpha.alpha(0.8);
             this.zombie2.alpha.alpha(0.2);
+            this.zombie3.alpha.alpha(1);
 
             this.addChild(this.zombie);
             this.addChild(this.zombie2);
+            this.addChild(this.zombie3);
         }
 
         update() {
 
             super.update();
-
+            if (this.zombie3.alpha.alpha() > 0)
+                this.zombie3.alpha.alpha(this.zombie3.alpha.alpha() - 0.01);
+            else
+                this.zombie3.alpha.alpha(1);
         }
 
 
