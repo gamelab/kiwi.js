0a762b4ef76d6bdd678f1b4f288e0a8d1b934fc0
Zachary Freiberg <zachary.freiberg@gmail.com>
2013-07-24 10:32:38 +1200

    Edit not working

--- examples/animations/CreatingAndSwitchingAnimations.ts
+++ examples/animations/CreatingAndSwitchingAnimations.ts
@@ -3,27 +3,52 @@
 class CreatingAndSwitchingAnimations extends Kiwi.State {
 
     constructor() {
-        super("CreatingAndSwitchingAnimations");
+        super("Creating_SwitchingAnimations");
+        
     }
 
-    public sprite: Kiwi.GameObjects.Sprite;
+    sprite: Kiwi.GameObjects.Sprite;
+
+    //mouse
+    myMouse: Kiwi.Input.Mouse;
 
     preload() {
 
         this.addSpriteSheet('coin', 'assets/coin.png', 32, 32);
+
     }
 
     create() {
 
         this.sprite = new Kiwi.GameObjects.Sprite('coin', this.game.cache, 100, 100);
+
         this.sprite.animation.add('rotateRight', 0.1, [0, 1, 2, 3, 4, 5], Kiwi.Anims.PLAY_LOOP);
         this.sprite.animation.add('rotateLeft', 0.1, [5, 4, 3, 2, 1, 0], Kiwi.Anims.PLAY_LOOP);
+
         this.sprite.animation.play('rotateRight');
+
         this.addChild(this.sprite);
+
+        
+
+        ////////////////////////////////////////
+        //Creating Mouse 
+        this.myMouse = this.game.input.mouse;
+        this.myMouse.start();
     }
 
     update() {
 
+
+
+
+        if (this.myMouse.justPressed()) {
+
+            this.sprite.animation.switchTo('rotateLeft');
+            console.log('test three zach');
+
+            this.myMouse.reset();
+        }
 
     }
 
