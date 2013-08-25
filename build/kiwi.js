9474667fb4ead5e8bbff116686a7ef40365152e9
Benjamin Harding <benjamin.p.harding@gmail.com>
2013-08-26 08:44:23 +1200

    Tag loader default setting when a blob support is detected corrected. Note: Tag loader doesn't work with texture atlases or images that are above a certain dimension.

--- build/kiwi.js
+++ build/kiwi.js
@@ -5154,7 +5154,7 @@ var Kiwi;
 
                 if (Kiwi.DEVICE.blob) {
                     klog.info('blob support found - using blob loader');
-                    this._useTagLoader = true;
+                    this._useTagLoader = false;
                 } else {
                     klog.info('blob support NOT found - using tag loader');
                     this._useTagLoader = true;
